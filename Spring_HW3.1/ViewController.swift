a82111d092d285610c2854e8e613a0666c7c3bc9
gaidaasd00 <swiftgaida@list.ru>
2022-08-15 17:58:26 +0300

    Initial Commit

---
+++ Spring_HW3.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spring_HW3.1
+//
+//  Created by Алексей Гайдуков on 15.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
